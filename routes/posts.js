da09526a2ace5b1f7b0c72af44092aed9487a43a
kavyareddyvanga <145844235+kavyareddyvanga@users.noreply.github.com>
2025-09-20 23:53:20 +0530

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -11,8 +11,8 @@ router.get("/:id", getPost);
 
 // Protected routes (require login)
 router.post("/",  addPost);          // only logged-in users can add
-router.put("/:id", verifyUser, updatePost);    // only owner can update
-router.delete("/:id", verifyUser, deletePost);// only owner can delete
+router.put("/:id", updatePost);    // only owner can update
+router.delete("/:id",  deletePost);// only owner can delete
 
 
 export default router;
